44e0b3fad7bf86be23c09a10550f6861b8b3bbe2
Tyago03 <146128433+Tyago03@users.noreply.github.com>
2023-11-22 12:44:40 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
 import speech_recognition as sr
-import re
 import pyttsx3
 import os
-import openai
 
 nome = "Tyago"
 
-openai.api_key = ' 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'
-
-while(True):
-
+while (True):
+    
     mic = sr.Recognizer()
-
+    
     with sr.Microphone() as source:
+        
         engine = pyttsx3.init()
         engine.setProperty('voice', "com.apple.speech.synthesis.voice.luciana")
-        mic.adjust_for_ambient_noise(source)
-        print("Fale alguma coisa: ")
+        mic.adjust_for_ambient_noise(source) 
+        
+        print("Fale alguma coisa:")
+        
         audio = mic.listen(source)
 
         try:
             frase = mic.recognize_google(audio, language='pt-BR')
+
+            # desligar / reiniciar o computador
+            if "desligue" or "desligar" in frase and "computador" in frase:
+                engine.say("desligando o computador")
+                engine.runAndWait()
+                os.system("shutdown -s -t 1")
+            elif "reinicie" or "reiniciar" in frase and "computador" in frase:
+                engine.say("reiniciando o computador")
+                engine.runAndWait()
+                os.system("shutdown -r -t 1")
+
+            # abrir programas
+            elif 'abrir' in frase and 'opera' in frase:
+                os.startfile("<caminho para o opera na sua máquina>")
+            elif 'abrir' in frase and 'discord' in frase:
+                os.startfile("<caminho para o discord na sua máquina>")
+            elif 'abrir' in frase and 'steam' in frase:
+                os.startfile("<caminho para a steam na sua máquina>")
+            elif 'abrir' in frase and 'vscode' in frase:
+                os.startfile("<caminho para o vscode na sua máquina>")
+
+            # checar temperatura, horário e data
+
+
+
+
+
+
             print(frase)
-
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=frase,
-                max_tokens=150
-            )
-            assistant_response = response['choices'][0]['text'].strip()
-            print(f"Resposta do assistente: {assistant_response}")
-
-            if (re.search("Abra a steam", format(frase))):
-                engine.say("Vou iniciar a Steam.")
-                os.startfile("Endereço da steam aqui")
-
-            elif (re.search("Abra o opera", format(frase))):
-                engine.say("Vou iniciar o Opera.")
-                os.startfile("Endereço do opera aqui")
-            else:
-                engine.say(assistant_response)
-
-
-        except sr.UnknowValueError:
-            print(f"Não entendi, pode repetir {nome}? ")
+        
+        except sr.UnknownValueError:
+            print("Não entendi. Dê o comando novamente:")
