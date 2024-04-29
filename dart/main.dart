e78250c4d05ad7629f64479bd784b9f8e0f64b3a
Tyago03 <146128433+Tyago03@users.noreply.github.com>
2024-04-29 11:07:02 -0300

    Update main.dart

--- dart/main.dart
+++ dart/main.dart
@@ -10,6 +10,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const MyApp());
 
+// Variável global da NavBar
 int currentIndex = 0;
 
 class PerguntaApp extends StatefulWidget {
